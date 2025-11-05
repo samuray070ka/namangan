d27a871f418eb1efc3ad0950831696054391ce8d
Muhammadsodiq <markelofmuhammadsodiq@gmail.com>
2025-11-05 16:42:45 +0500

    change-7

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -18,44 +18,7 @@ function Home() {
   //   name: t("district_name"),
   //   updated: t("last_updated"),
   // });
-  const tumanlar = [
-    {
-      slog: "slog1",
-      id: 1,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    },
-    {
-      slog: "slog2",
-      id: 2,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    },
-    {
-      slog: "slog3",
-      id: 3,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    },
-    {
-      slog: "slog4",
-      id: 4,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    },
-    {
-      slog: "slog5",
-      id: 5,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    },
-    {
-      slog: "slog6",
-      id: 6,
-      title: "Поп миршикори мчж",
-      desc: "Дата изменения | 18:37"
-    }
-  ]
+
 
   return (
     <div className="home">
